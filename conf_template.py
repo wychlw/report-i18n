13352ce6ed07a3bda43075c8ded733166f6ba2d7
Ling Wang <lingwang@wcysite.com>
2024-06-13 12:08:12 +0800

    Specilize

---
+++ conf_template.py
@@ -0,0 +1,19 @@
+max_length = 2000
+
+target_lang = {
+    "en": "English",
+}
+
+dir_to_translate = "dir_to_translate"
+dir_translated = {
+    "en": "englist_translated_dir",
+}
+
+exclude_list = []
+processed_list = "processed_list.txt"
+
+tips_translated_by_chatgpt = {
+    "en": "\n\n> This doc was automatically translated by GPT and has not been proofread yet. Please give us feedback in issue if any omissions.\n"
+}
+
+template = ""
