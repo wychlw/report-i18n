d82abe13d82c6963d6db2ce83e5c9ce1077c42b0
Ling Wang <lingwang@wcysite.com>
2024-07-03 01:33:32 +0800

    Add function: auto translate to README_[lang].md, tweak param, remove processed list

--- auto-translater.py
+++ auto-translater.py
@@ -11,14 +11,8 @@ client = openai.OpenAI(
     # base_url=os.environ.get("CHATGPT_API_BASE")
 )
 
-
-
-# 文章使用英文撰写的提示，避免本身为英文的文章被重复翻译为英文
-marker_written_in_en = "\n[english]\n"
 # 即使在已处理的列表中，仍需要重新翻译的标记
 marker_force_translate = "\n[translate]\n"
-
-
 
 def translate_text(paragraths, lang):
     target_promote = target_lang[lang]
@@ -40,19 +34,17 @@ def translate_text(paragraths, lang):
     # 这是必要的。否则很多会容易出现中间截断及上下用词不统一。我们需要上下文。
     for paragraph in paragraths:
         promote.append({"role": "user", "content": f"Continue translate into {target_promote}:\n\n{paragraph}\n"})
-        # completion = client.chat.completions.create(
-        #     model="gpt-3.5-turbo", # 用 4o 会更好，但是贵啊——
-        #     messages=promote,
-        # )
+
+        max_token = 4095 if model == "gpt-4o" else 2048
+
         completion = client.chat.completions.create(
-            # model="gpt-3.5-turbo",
-            model="gpt-4o",
+            model=model,
             messages=promote,
-            # temperature=0,
-            # max_tokens=4096,
-            # top_p=1,
-            # frequency_penalty=0,
-            # presence_penalty=0
+            temperature=0.2,
+            max_tokens=max_token,
+            top_p=0.5,
+            frequency_penalty=0,
+            presence_penalty=0
         )
         promote.append({"role": "assistant", "content": completion.choices[0].message.content})
         output_text = completion.choices[0].message.content
@@ -60,7 +52,6 @@ def translate_text(paragraths, lang):
 
     return res
 
-# 定义文章拆分函数
 def split_text(text: str, max_length):
     # 拆分文章
     paragraphs = text.split("\n\n")
@@ -94,19 +85,7 @@ def split_text(text: str, max_length):
 
     return translation_paragraphs
 
-# 定义翻译文件的函数
-def translate_file(input_file, filename, lang):
-    print(f"Translating into {lang}: {filename}")
-    sys.stdout.flush()
-
-    # 定义输出文件
-    if lang in dir_translated:
-        output_dir = dir_translated[lang]
-        output_file = os.path.join(output_dir, filename)
-        real_dir = os.path.dirname(output_file)
-        if not os.path.exists(real_dir):
-            os.makedirs(real_dir)
-
+def translate_file(input_file, lang):
 
     # 读取输入文件内容
     with open(input_file, "r", encoding="utf-8") as f:
@@ -115,9 +94,6 @@ def translate_file(input_file, filename, lang):
     # 删除译文中指示强制翻译的 marker
     input_text = input_text.replace(marker_force_translate, "")
 
-    # 删除其他出英文外其他语言译文中的 marker_written_in_en
-    if lang != "en":
-        input_text = input_text.replace(marker_written_in_en, "")
 
     # 对于
     # ```.*
@@ -143,12 +119,9 @@ def translate_file(input_file, filename, lang):
         output_text = output_text.replace(f"[CODEBLOCK[{idx}]]", code_block)
 
     # 加入由 ChatGPT 翻译的提示
-    if lang == "en":
-        output_text = output_text + tips_translated_by_chatgpt["en"]
+    output_text = output_text + tips_translated_by_chatgpt[lang]
 
-    # 写入输出文件
-    with open(output_file, "w", encoding="utf-8") as f:
-        f.write(output_text)
+    return output_text
 
 # 按文件名称顺序排序，去除某些目录
 # 会递归的进行处理
@@ -156,8 +129,6 @@ file_list:list[str] = []
 for root, dirs, files in os.walk(dir_to_translate):
     if ".git" in dirs:
         dirs.remove(".git")
-    if "i18n" in dirs:
-        dirs.remove("i18n")
     for file in files:
         if file.endswith(".md"):
             file_list.append(os.path.join(root, file))
@@ -165,48 +136,34 @@ sorted_file_list = sorted(file_list, key=lambda x: x.lower())
 
 
 try:
-    # 创建一个外部列表文件，存放已处理的 Markdown 文件名列表
-    if not os.path.exists(processed_list):
-        # 如果文件不存在，则创建一个新文件
-        with open(processed_list, "w", encoding="utf-8") as f:
-            print("processed_list created")
-            sys.stdout.flush()
-
-    with open(processed_list, "r", encoding="utf-8") as f:
-        processed_list_content = f.read().splitlines()
 
     # 遍历目录下的所有.md 文件，并进行翻译
     for input_file in sorted_file_list:
-        filename = input_file.replace(dir_to_translate + '/', "")
+        
+        filename = os.path.basename(input_file)
+        filedir = os.path.dirname(input_file)
+        
+        if filename != "README.md":
+            continue
+
+        if filename in exclude_list:
+            continue
 
         # 读取 Markdown 文件的内容
         with open(input_file, "r", encoding="utf-8") as f:
             md_content = f.read()
 
-        if marker_force_translate in md_content:  # 如果有强制翻译的标识，则执行这部分的代码
-            if marker_written_in_en in md_content:  # 翻译为除英文之外的语言
-                print("Pass the en-en translation: ", filename)
-                sys.stdout.flush()
-            else:  # 翻译为所有语言
-                translate_file(input_file, filename, "en")
-        elif filename in exclude_list:  # 不进行翻译
-            print(f"Pass the post in exclude_list: {filename}")
-            sys.stdout.flush()
-        elif filename in processed_list_content:  # 不进行翻译
-            print(f"Pass the post in processed_list: {filename}")
-            sys.stdout.flush()
-        elif marker_written_in_en in md_content:  # 翻译为除英文之外的语言
-            print(f"Pass the en-en translation: {filename}")
-            sys.stdout.flush()
-        else:  # 翻译为所有语言
-            for lang, _ in target_lang.items():
-                translate_file(input_file, filename, lang)
+        for lang, _ in target_lang.items():
+            target_file = os.path.join(filedir, "README_" + lang + ".md" )
+            if target_file in file_list and marker_force_translate not in md_content:
+                continue
 
-        if filename not in processed_list_content:
-            print(f"Added into processed_list: {filename}")
-            with open(processed_list, "a", encoding="utf-8") as f:
-                f.write("\n")
-                f.write(filename)
+            print(f"Translating into {lang}: {input_file}")
+            sys.stdout.flush()
+            output_content = translate_file(input_file, lang)
+                # 写入输出文件
+            with open(target_file, "w", encoding="utf-8") as f:
+                f.write(output_content)
 
         sys.stdout.flush()
         
