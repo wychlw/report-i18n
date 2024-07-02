81142e362626abac3e7e23480ae959ae6ed41b7b
Ling Wang <lingwang@wcysite.com>
2024-07-03 01:51:20 +0800

    Fix: template

--- conf_template.py
+++ conf_template.py
@@ -1,19 +1,20 @@
-max_length = 2000
+max_length = 4095
+
+model="gpt-3.5-turbo",
+# model="gpt-4o"
 
 target_lang = {
     "en": "English",
+    # "fr": "French"
 }
 
-dir_to_translate = "dir_to_translate"
-dir_translated = {
-    "en": "englist_translated_dir",
-}
+dir_to_translate = "/path/to/dir"
 
 exclude_list = []
-processed_list = "processed_list.txt"
 
 tips_translated_by_chatgpt = {
     "en": "\n\n> This doc was automatically translated by GPT and has not been proofread yet. Please give us feedback in issue if any omissions.\n"
+    # "fr": "\n\n> Ce document a été traduit automatiquement par GPT et n'a pas encore été relu. Veuillez nous faire part de vos commentaires en cas d'omission.\n"
 }
 
 template = ""
