0352a2d6e65057dcc32c9d28e561b2bea4c0b709
Ling Wang <lingwang@wcysite.com>
2024-07-03 01:36:34 +0800

    Fix: CI

--- env_template.py
+++ env_template.py
@@ -1,3 +1,5 @@
 import os
 os.environ["CHATGPT_API_KEY"] = "sk-xxxx" 
-os.environ["CHATGPT_API_BASE"] = "xxx" # e.g. 官方API地址：https://api.openai.com/v1/
+os.environ["CHATGPT_API_BASE"] = "xxx" # 官方 API 地址：https://api.openai.com/v1/
+
+# !!! 若要使用 CI，注释掉上面两行并把东西放在 CI 的 secret 里
